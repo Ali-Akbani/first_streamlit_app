2da6b674cde17295c19a644175d1fce632eb8060
Ali-Akbani <Ali-Akbani@users.noreply.github.com>
2022-12-15 21:09:43 +1100

    fixed get_fruityvice_data function

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #New section to display FruityVice API response
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     #streamlit.text(fruityvice_response.json())
     # Normalises the JSON response
