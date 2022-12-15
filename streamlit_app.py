5c5be76f3c04fe97a52500328aa76b25a4b56534
Ali-Akbani <Ali-Akbani@users.noreply.github.com>
2022-12-15 20:49:08 +1100

    Added error handling to FruityVice API section

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,18 +27,21 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display FruityVice API response
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+     streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #streamlit.text(fruityvice_response.json())
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# Normalises the JSON response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#Converts data into a table
-streamlit.dataframe(fruityvice_normalized)
-
+    # Normalises the JSON response
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    #Converts data into a table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+  
 streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
