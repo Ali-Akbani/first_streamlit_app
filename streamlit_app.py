d5a92f34e89f3351c5f843773c341fe3ccdee951
Ali-Akbani <Ali-Akbani@users.noreply.github.com>
2022-12-15 20:31:40 +1100

    Added prompt to allow user to add a fruit to fruit_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,6 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
+
+#Prompt user to add their own fruit
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
